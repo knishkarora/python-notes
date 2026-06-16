590c27b14a77b4dabf5f6ee65ea1e502ef0f0441
Knishk Arora <knishkarora.dev@gmail.com>
2026-06-17 00:46:53 +0530

    prac: classes and objects topic covered with practice questions

---
+++ dunder.py
@@ -0,0 +1,22 @@
+class Book:
+   def __init__(self, title, pages):
+       self.title = title
+       self.pages = pages
+
+   def __len__(self):
+       return self.pages
+
+   def __str__(self):
+       return f"'{self.title}' has {self.pages} pages"
+
+   def __eq__(self, other):
+       return self.pages == other.pages
+  
+book1 = Book("Built Wealth Like a Boss", 420)
+book2 = Book("Be Your Own Start", 420)
+
+print(len(book1)) # 420
+print(len(book2)) # 420
+print(str(book1)) # 'Built Wealth Like a Boss' has 420 pages
+print(str(book2)) # 'Be Your Own Start' has 420 pages
+print(book1 == book2) # True
